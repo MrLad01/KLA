97188ea4470d90d1b94e9bd27fecb785245f8bac
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-03 23:07:06 +0100

    update

--- frontend/src/components/Cont.jsx
+++ frontend/src/components/Cont.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cont = () => {
   return (
     <React.Fragment>
-        <div className='bg-background object-cover'>
+        <div className='bg-background1 object-cover w-screen h-screen'>
             <h2 className='text-white'>CONTACT US</h2>
         </div>
     </React.Fragment>
