652fbc5656b143da24b29911d449c419cc7ec530
MrLad01 <ladayodeji@gmail.com>
2023-08-03 22:54:16 +0100

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div className='bg-background h-152 w-full page-1 flex items-center justify-center'>
+      <div className='bg-background1 h-152 w-full page-1 flex items-center justify-center'>
         {/* <h1> HANDMADE PERFECTION</h1> */}
         <h1>STYLIST PICKS BEAT <br /> THE HEAT</h1>
 
