c7fb294bcff7daf4ff170c4585a5a2f941cbb070
MrLad01 <ladayodeji@gmail.com>
2023-08-03 23:04:49 +0100

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div>
       <div className='bg-background h-152 w-full page-1 flex items-center justify-center flex-col'>
         {/* <h1> HANDMADE PERFECTION</h1> */}
-        <h1 className='-mt-16'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
+        <h1 className='-mt-24'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
         <button>SHOP NOW</button>
       </div>
     </div>
