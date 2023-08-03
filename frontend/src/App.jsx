e5767492478a293dfd2d13d221f18a3134c79177
MrLad01 <ladayodeji@gmail.com>
2023-08-03 21:32:07 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import Nav from "./layout/Nav"
 
 
 function App() {
@@ -6,7 +7,9 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-
+          <Route path="/" element={<Nav />}>
+            
+          </Route>
       </Route>
     )
   )
