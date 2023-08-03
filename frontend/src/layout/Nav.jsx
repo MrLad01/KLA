fa4e1dc4c13344e64b9c79f1f9f21c66621b2aeb
MrLad01 <ladayodeji@gmail.com>
2023-08-03 22:20:22 +0100

    update

--- frontend/src/layout/Nav.jsx
+++ frontend/src/layout/Nav.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import logo from '../assets/KLA logo.png'
-import {  NavLink } from 'react-router-dom'
+import {  NavLink, Outlet } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <nav className='flex flex-row w-full bg-black text-white'>
-      <img src={logo} alt="KLA logo" />
-      <div>
-        <NavLink>Home</NavLink>
-        <NavLink>About</NavLink>
-        <NavLink>Contact Us</NavLink>
-      </div>
-    </nav>
+    <>
+     <nav className='flex w-full'>
+        <img src={logo} alt="KLA logo" />
+        <div className='justify-center'>
+            <NavLink>Home</NavLink>
+            <NavLink>About</NavLink>
+            <NavLink>Contact Us</NavLink>
+        </div>
+     </nav>
+     <Outlet />
+    </>
   )
 }
 
