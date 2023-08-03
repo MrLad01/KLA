5db434d9a1f6d517d81c5bfc2ed6eb32b8f4ce54
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-03 22:57:17 +0100

    update

---
+++ frontend/src/components/Cont.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cont = () => {
+  return (
+    <React.Fragment>
+        <div className='bg-background object-cover'>
+            <h2 className='text-white'>CONTACT US</h2>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Cont
---
+++ frontend/src/components/Formpage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Form } from 'react-router-dom'
+
+const Formpage = () => {
+  return (
+    <React.Fragment>
+        <div>
+            <header>
+            <h3>We would love to hear from you.</h3>
+            <p>If you have any query or any type of           suggestion,  you can contact us here. We would love to hear from you.</p>
+            </header>
+        <section>
+        <Form>
+            <label htmlFor="name">Name
+            <input type="text" />
+            </label>
+            <label htmlFor="name">Email
+            <input type="email" />
+            </label>
+            <label htmlFor="name">Message
+            <input type="text" />
+            </label>
+            <button type='submit'>SEND MESSAGE</button>
+        </Form>
+        <aside>
+            <div>
+            <h5>Visit Us</h5>
+            <p>FUTA SOUTHGATE <br /> +2349065765454</p>
+            </div>
+            <div>
+                <h5>Get In Touch</h5>
+                <p>You can get in touch with us on this provided email.</p>
+                <p>Email: hmjawad087@gmail.com</p>
+            </div>
+        </aside>
+      </section>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Formpage
