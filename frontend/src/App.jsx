9e9208c1261a2d362e9032e3c65c1af8d9a54386
MrLad01 <ladayodeji@gmail.com>
2023-08-03 21:19:55 +0100

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+
+
+function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+
+      </Route>
+    )
+  )
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
