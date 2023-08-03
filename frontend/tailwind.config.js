4a3636a43cece41b15c26fc486cd14e00252b4fb
MrLad01 <ladayodeji@gmail.com>
2023-08-03 22:17:08 +0100

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./src/layout/Nav.jsx",
     "./src/pages/About.jsx",
-    "./src/pages/Home.jsx",
     "./src/pages/Contact.jsx",
-    "./src/layout/Nav.jsx"
+    "./src/pages/Home.jsx"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
