80023ee14ac0a03b8c928075c9d7ab160bafcd54
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-03 23:12:13 +0100

    update

--- frontend/src/components/Cont.jsx
+++ frontend/src/components/Cont.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Cont = () => {
   return (
     <React.Fragment>
         <div className='bg-background1 object-cover w-screen h-screen'>
-            <h2 className='text-white'>CONTACT US</h2>
+            <Link className='text-white'>CONTACT US</Link>
         </div>
     </React.Fragment>
   )
