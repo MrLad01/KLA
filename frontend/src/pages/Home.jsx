f64f2db5bfa72603d1cfac01c40e77613c442217
MrLad01 <ladayodeji@gmail.com>
2023-08-03 23:20:02 +0100

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div>
       <div className='bg-background h-152 w-full page-1 flex items-center justify-center flex-col'>
         {/* <h1> HANDMADE PERFECTION</h1> */}
-        <h1 className='-mt-28'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
-        <button>SHOP NOW</button>
+        <h1 className='-mt-16'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
+        <button className='border-4 border-white py-2 px-5 mt-16 hover:bg-white text-inherit '>SHOP NOW</button>
       </div>
     </div>
   )
