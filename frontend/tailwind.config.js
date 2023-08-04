30f72e721d780256dd0abb9e0e46a9b1c8c70187
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-04 08:39:10 +0100

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,16 +5,8 @@ export default {
     "./src/pages/About.jsx",
     "./src/pages/Contact.jsx",
     "./src/pages/Home.jsx",
-<<<<<<< HEAD
     ".src/components/Cont.jsx",
     ".src/components/FormPage.jsx"
-=======
-    "./src/components/Cont.jsx",
-    "./src/components/FormPage.jsx",
-    "./src/pages/SubPages/Page1.jsx",
-    "./src/pages/SubPages/Page2.jsx",
-    "./src/pages/SubPages/Page3.jsx"
->>>>>>> 4a5a84524035eaccc705d58484a811946fca3431
   ],
   theme: {
     extend: {
@@ -23,18 +15,7 @@ export default {
         'background' : "url('./assets/img_1.png')",
       },
       height: {
-        '100': '25rem',
-        '152': '38rem',
-        '162': '40.5rem',
-        '182': '45.5rem',
-        '230': '57.5rem'
-      },
-      width: {
-        '176': '44rem'
-      },
-      colors: {
-        'primary3' : '#024E82',
-        'secondary3': '#47AEF5'
+        '152': '38rem'
       }
     },
   },
