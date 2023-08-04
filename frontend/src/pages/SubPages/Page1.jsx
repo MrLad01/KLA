e80581ff473b0ccb8b3f8b20eab581d4dc084bb4
MrLad01 <ladayodeji@gmail.com>
2023-08-04 08:33:56 +0100

    update

--- frontend/src/pages/SubPages/Page1.jsx
+++ frontend/src/pages/SubPages/Page1.jsx
@@ -6,7 +6,7 @@ const Page1 = () => {
         <div className='bg-background h-152 w-full page-1 flex items-center justify-center flex-col'>
         {/* <h1> HANDMADE PERFECTION</h1> */}
         <h1 className='-mt-16'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
-        <button className='border-4 border-white py-2 px-5 mt-14 hover:text-base hover:py-2.5 hover:px-6  hover:transition duration-200 hover:transition-delay-100 hover:mt-16'>SHOP NOW</button>
+        <button className='border-4 border-white py-2 px-5 mt-16 hover:text-base hover:py-2.5 hover:px-6  hover:transition duration-200 hover:transition-delay-100'>SHOP NOW</button>
       </div>
     </React.Fragment>
   )
--- frontend/src/pages/SubPages/Page3.jsx
+++ frontend/src/pages/SubPages/Page3.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import icon1 from "../../assets/Home Page 3 icons/icon 1.png"
+import icon2 from "../../assets/Home Page 3 icons/icon 2.png"
+import icon3 from "../../assets/Home Page 3 icons/icon 3.png"
+import icon4 from "../../assets/Home Page 3 icons/icon 4.png"
 
 const Page3 = () => {
   return (
     <React.Fragment>
       <div className='page-3 h-152 w-full'>
-        Hello
+        <div className='grid grid-cols-4 grid-rows-1 mx-20'>
+            <div className='flex'>
+                <img src={icon1} alt="" />
+                <div className='grid'>
+                    <h4>FREE DELIVERY</h4>
+                    <h5>Enjoy Delivery from anywhere nationwide</h5>   
+                </div>
+            </div>
+            <div className='flex'>
+                <img src={icon2} alt="" />
+                <div className='grid'>
+                    <h4>SUPPORT 24/7</h4>
+                    <h5>Our support team is there to help you for enquiries</h5>
+                </div>
+            </div>
+            <div className='flex'>
+                <img src={icon3} alt="" />
+                <div className='grid'>
+                    <h4>30 DAYS RETURN</h4>
+                    <h5>Simply return it within 30 days of an exchange</h5>
+                </div>
+            </div>
+            <div className='flex'>
+                <img src={icon4} alt="" />
+                <div className='grid'>
+                    <h4>100% PAYMENT SECURE</h4>
+                    <h5>Our payments are secured with 256 bit encryption</h5>
+                </div>
+            </div>
+        </div>
       </div>
     </React.Fragment>
   )
