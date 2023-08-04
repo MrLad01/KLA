402188d67c5fbdc0f03c7d4e89fd2b9c518ff4a4
MrLad01 <ladayodeji@gmail.com>
2023-08-04 05:32:26 +0100

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='bg-background h-152 w-full page-1 flex items-center justify-center flex-col'>
         {/* <h1> HANDMADE PERFECTION</h1> */}
         <h1 className='-mt-16'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
-        <button className='border-4 border-white py-2 px-5 mt-16 hover:bg-white text-inherit '>SHOP NOW</button>
+        <button className='border-4 border-white py-2 px-5 mt-16 hover:bg-white  hover:text-transparent hover:bg-clip-text '>SHOP NOW</button>
       </div>
     </div>
   )
