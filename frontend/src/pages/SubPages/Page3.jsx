776a6e9ea0715e0252fd2bb9a2c26b3ea9737e59
MrLad01 <ladayodeji@gmail.com>
2023-08-04 09:49:19 +0100

    update

--- frontend/src/pages/SubPages/Page3.jsx
+++ frontend/src/pages/SubPages/Page3.jsx
@@ -42,12 +42,12 @@ const Page3 = () => {
             <div className='h-full bg-primary3 w-full rounded-2xl justify-center items-center flex flex-col'>
                 <header>PEACE OF MIND</header>
                 <p>A one-stop platform for all your fashion needs, hassle-free. Buy with a peace of mind.</p>
-                <button>ORDER NOW</button>
+                <button className='text-primary3'>ORDER NOW</button>
             </div>
             <div className='h-full bg-secondary3 w-176 rounded-2xl justify-center items-center flex flex-col'>
                 <header>BUY 2, GET 1 FREE</header>
                 <p>End of season sale. Buy any 2 items of your choice and get 1 free.</p>
-                <button>ORDER NOW</button>
+                <button className='text-secondary3'>ORDER NOW</button>
             </div>
         </div>
       </div>
