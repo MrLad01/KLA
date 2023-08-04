17830e8aa28f5c523926e519d621fc4a980b9089
MrLad01 <ladayodeji@gmail.com>
2023-08-04 06:33:34 +0100

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='h-full'>
       <div className='bg-background h-152 w-full page-1 flex items-center justify-center flex-col'>
         {/* <h1> HANDMADE PERFECTION</h1> */}
         <h1 className='-mt-16'>STYLIST PICKS BEAT <br /> THE HEAT</h1>
-        <button className='border-4 border-white py-2 px-5 mt-16 hover:text-lg hover:py-3 hover:px-6  hover:transition duration-300 hover:transition-delay-0'>SHOP NOW</button>
+        <button className='border-4 border-white py-2 px-5 mt-14 hover:text-base hover:py-2.5 hover:px-6  hover:transition duration-200 hover:transition-delay-100 hover:mt-16'>SHOP NOW</button>
 
 
       </div>
