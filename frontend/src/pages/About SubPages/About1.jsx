b9d0e3fd71c72fd04c00782dd729573ed581a288
MrLad01 <ladayodeji@gmail.com>
2023-08-04 17:24:51 +0100

    update

--- frontend/src/pages/About SubPages/About1.jsx
+++ frontend/src/pages/About SubPages/About1.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const About1 = () => {
   return (
     <React.Fragment>
-        <div className="bg-aboutBack w-full h-86">
-
+        <div className="bg-aboutBack w-full h-56 about1">
+            <h3 className='absolute top-48 ml-16 text-white'>KHAFI_LEATHERAFFAIRS
+            </h3>
         </div>
     </React.Fragment>
   )
