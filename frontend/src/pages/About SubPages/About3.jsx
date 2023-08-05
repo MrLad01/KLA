294fceeeaad67b5ef6b03b60140067c04f9560bf
MrLad01 <ladayodeji@gmail.com>
2023-08-05 07:27:56 +0100

    update

--- frontend/src/pages/About SubPages/About3.jsx
+++ frontend/src/pages/About SubPages/About3.jsx
@@ -5,7 +5,24 @@ const About3 = () => {
     <React.Fragment>
         <div className="footer h-152 w-full bg-footer mb-20 about3 pt-16">
             <header className='mb-20'>The Gallery</header>
-            <div className='flex mx-20 gap-10'></div>
+            <div className='flex mx-20 gap-10'>
+              <div>
+                <img src="" alt="" />
+                <h4></h4>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <h4></h4>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <h4></h4>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <h4></h4>
+              </div>
+            </div>
         </div>
     </React.Fragment>
   )
