935cdb42452fb869a9bda229176b275023acb1be
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-05 11:22:50 +0100

    update

--- frontend/src/layout/Nav.jsx
+++ frontend/src/layout/Nav.jsx
@@ -8,10 +8,13 @@ const Nav = () => {
     <>
      <nav className='flex w-full my-2'>
         <img src={logo} alt="KLA logo" className='ml-2'/>
-        <div className='justify-center'>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="about">About</NavLink>
-            <NavLink to="contact">Contact Us</NavLink>
+        <div className='justify-center ml-16 mt-4'>
+            <NavLink className="ml-9 font-arimo font-bold text-base text-center" to="/">Home</NavLink>
+            <NavLink className="ml-9 font-arimo font-bold text-base text-center" to="about">About</NavLink>
+            <NavLink className="ml-9 font-arimo font-bold text-base text-center" to="contact">Contact Us</NavLink>
+        </div>
+        <div>
+
         </div>
      </nav>
      <Outlet />
