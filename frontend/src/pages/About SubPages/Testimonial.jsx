fddfbd8ac11549965c35fb609bcb9b03b7ce51c2
MrLad01 <ladayodeji@gmail.com>
2023-08-05 07:50:55 +0100

    update

--- frontend/src/pages/About SubPages/Testimonial.jsx
+++ frontend/src/pages/About SubPages/Testimonial.jsx
@@ -3,7 +3,35 @@ import React from 'react'
 const Testimonial = () => {
   return (
     <React.Fragment>
-      
+      <div className="testimonial h-360 w-full">
+        <header>Testimonial</header>
+        <div className='mx-56 grid mt-28 gap-y-56'>
+            <div className="flex">
+                <img src="" alt="" />
+                <div>
+                    <img src="" alt="" />
+                    <p></p>
+                    <h3></h3>
+                </div>
+            </div>
+            <div className="flex">
+                <img src="" alt="" />
+                <div>
+                    <img src="" alt="" />
+                    <p></p>
+                    <h3></h3>
+                </div>
+            </div>
+            <div className="flex">
+                <img src="" alt="" />
+                <div>
+                    <img src="" alt="" />
+                    <p></p>
+                    <h3></h3>
+                </div>
+            </div>
+        </div>
+      </div>
     </React.Fragment>
   )
 }
