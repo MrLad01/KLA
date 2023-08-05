73de6803dba84dffe1af58885132d323ad6e1777
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-05 15:01:22 +0100

    update

--- frontend/src/layout/Nav.jsx
+++ frontend/src/layout/Nav.jsx
@@ -9,19 +9,19 @@ import menu from '../assets/menu 1.png'
 const Nav = () => {
   return (
     <>
-     <nav className='flex items-center justify-between w-full my-2 nav'>
+     <nav className='flex w-full my-2 nav'>
       <div className='flex'>
         <img src={logo} alt="KLA logo" className='ml-2'/><p className='font-arimo mt-8 text-xs italic text-gray4'>Handmade Perfection...</p>
         </div>
-        <div className='mt-4'>
+        <div className='mt-4  ml-48'>
             <NavLink className="font-arimo font-bold text-base text-center hover:text-header" to="/">Home</NavLink>
             <NavLink className="ml-9 font-arimo font-bold text-base text-center hover:text-header" to="about">About</NavLink>
             <NavLink className="ml-9 font-arimo font-bold text-base text-center hover:text-header" to="contact">Contact Us</NavLink>
         </div>
-        <div className='flex justify-end'>
-          <NavLink to=''><img src={user} alt="user icon" /></NavLink>
-          <NavLink to=''><img src={cart} alt="cart icon" /></NavLink>
-          <NavLink to=''><img src={menu} alt="menu icon" /></NavLink>
+        <div className='flex ml-auto'>
+          <NavLink to='' ><img src={user} alt="user icon" /></NavLink>
+          <NavLink to='' className='ml-2 h-'><img src={cart} alt="cart icon" /></NavLink>
+          <NavLink to='' className='ml-2'><img src={menu} alt="menu icon" /></NavLink>
         </div>
      </nav>
      <Outlet />
