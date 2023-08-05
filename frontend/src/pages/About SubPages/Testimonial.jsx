f23d6f72b61466caecfad38135029cc3b6bc5827
MrLad01 <ladayodeji@gmail.com>
2023-08-05 08:01:45 +0100

    update

--- frontend/src/pages/About SubPages/Testimonial.jsx
+++ frontend/src/pages/About SubPages/Testimonial.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import img1 from "../../assets/About Page/img 7.png"
+import img2 from "../../assets/About Page/img 8.png"
+import img3 from "../../assets/About Page/img 9.png"
+import qoutes from "../../assets/About Page/qoutation.png"
 
 const Testimonial = () => {
   return (
@@ -7,27 +11,27 @@ const Testimonial = () => {
         <header>Testimonial</header>
         <div className='mx-56 grid mt-28 gap-y-56'>
             <div className="flex">
-                <img src="" alt="" />
-                <div>
-                    <img src="" alt="" />
-                    <p></p>
-                    <h3></h3>
+                <img src={img1} alt="" />
+                <div className='ml-6'>
+                    <img src={qoutes} alt="" />
+                    <p className="mt-2">Once we ordered some accessories items and we got the products delivered in our doorstep, the customer support was super helpful and they answered all my queries.</p>
+                    <h3 className="mt-6">Stacy</h3>
                 </div>
             </div>
             <div className="flex">
-                <img src="" alt="" />
-                <div>
-                    <img src="" alt="" />
-                    <p></p>
-                    <h3></h3>
+                <img src={img2} alt="" />
+                <div className='ml-6'>
+                    <img src={qoutes} alt="" />
+                    <p className="mt-2">I ordered 5 shirts from them and received them in no time. The customer support was awesome!</p>
+                    <h3 className="mt-6">Tiffany</h3>
                 </div>
             </div>
             <div className="flex">
-                <img src="" alt="" />
-                <div>
-                    <img src="" alt="" />
-                    <p></p>
-                    <h3></h3>
+                <img src={img3} alt="" />
+                <div className='ml-6'>
+                    <img src={qoutes} alt="" />
+                    <p className="mt-2">I got a wrong shirt so I contacted them and they happily offered me a refund. I will surely shop from them again.</p>
+                    <h3 className="mt-6">James</h3>
                 </div>
             </div>
         </div>
