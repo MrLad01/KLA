4b370c3fb4d64b90d7623a5694737855b38832c8
MrLad01 <ladayodeji@gmail.com>
2023-08-05 08:54:31 +0100

    update

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,21 +9,29 @@ const Contact = () => {
           <h3 className='absolute top-48 ml-16 text-white'>CONTACT US</h3>
         </div> 
         <section className='contact2 mt-24 ml-10 h-200'>
-            <h3 className='font-arimo text-tertiary font-bold text-4xl'>We would love to hear from you.</h3>
-            <p className='font-lato text-tertiary font-normal text-base'>If you have any query or any type of suggestion, you can contact us here. We would love to hear from you.</p>
+            <h3 className=' text-tertiary font-bold text-4xl mb-6'>We would love to hear from you.</h3>
+            <p className='font-lato text-tertiary font-normal text-base mb-10'>If you have any query or any type of suggestion, you can contact us here. We would love to hear from you.</p>
         <Form className='grid'>
-            <label htmlFor="name">Name</label>
-            <input type="text" id='name' />
+            <div className='flex'>
+              <div className='grid mr-4'>
+                <label htmlFor="name">Name</label>
+                <input type="text" id='name' />
+              </div>
 
-            <label htmlFor="email">Email</label>
-            <input type="email" id='email' />
+              <div className='grid'>
+                <label htmlFor="email">Email</label>
+                <input type="email" id='email' />
+              </div>
+
+            </div>
       
             <label htmlFor="message">Message</label>
             <textarea name="" id="" cols="2" rows="2"></textarea>
        
             <button type='submit'>SEND MESSAGE</button>
         </Form>
-        <aside className='absolute right-10 -mt-96'>
+
+        <aside className='absolute right-10 top-96'>
             <div>
             <h5>Visit Us</h5>
             <p>FUTA SOUTHGATE <br /> +2349065765454</p>
