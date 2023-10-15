564c086bd3466a704268acf9027784e181f39601
MrLad01 <ladayodeji@gmail.com>
2023-10-15 20:26:53 +0100

    update

--- frontend/src/pages/About SubPages/About1.jsx
+++ frontend/src/pages/About SubPages/About1.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const About1 = () => {
   return (
     <React.Fragment>
-        <div className="bg-aboutBack w-full h-56 about1">
-            <h3 className='absolute top-48 ml-16 text-white'>KHAFI_LEATHERAFFAIRS
-            </h3>
-        </div>
+        
+        <div className = "bg-aboutBack w-full h-56 about1">
+            <h3 className = 'absolute top-48 ml-16 text-white'> KHAFI_LEATHERAFFAIRS </h3>
+        </div> 
+
     </React.Fragment>
   )
 }
--- frontend/src/pages/About SubPages/About2.jsx
+++ frontend/src/pages/About SubPages/About2.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 const About2 = () => {
   return (      
     <React.Fragment>
-       <div className='mx-20 flex gap-10 mt-20 mb-20'>
-            <div className='bg-aboutImg1 h-144 w-144 items-end flex justify-center'>
-                <button className='bg-white text-primary3 flex relative bottom-20 py-3 px-8 rounded-2xl'>ORDER NOW</button>
+       
+       <div className = 'mx-20 flex gap-10 mt-20 mb-20'>
+            <div className = 'bg-aboutImg1 h-144 w-144 items-end flex justify-center'>
+                <button className = 'bg-white text-primary3 flex relative bottom-20 py-3 px-8 rounded-2xl'> ORDER NOW </button>
             </div>
-            <div className='bg-aboutImg2 h-144 w-144 items-end flex justify-center'>
-                <button className='bg-white text-primary3 flex relative bottom-20 py-3 px-8 rounded-2xl'>ORDER NOW</button>
+
+            <div className = 'bg-aboutImg2 h-144 w-144 items-end flex justify-center'>
+                <button className = 'bg-white text-primary3 flex relative bottom-20 py-3 px-8 rounded-2xl'> ORDER NOW </button>
             </div>
-        </div>     
+
+        </div> 
+
     </React.Fragment>
   )
 }
